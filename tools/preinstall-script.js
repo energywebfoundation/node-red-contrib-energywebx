4fdf3d45012379e64d9732dd190cdd87e1bb31fa
Krzysztof Szostak <christopher.szostak@energyweb.org>
2024-05-28 11:57:03 +0200

    chore: remove yarn requirement

--- tools/preinstall-script.js
+++
@@ -1,5 +0,0 @@
-if (process.env.npm_execpath.indexOf('yarn') === -1) {
-    console.error('You must use Yarn to install dependencies:');
-    console.error('  $ yarn install');
-    process.exit(1);
-}
